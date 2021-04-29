83226407062320f2865af5f3ee3f6c3bce94e66b
Kevin Messali <kevinmessali06@gmail.com>
2021-04-29 20:43:06 +0200

    Finishing Question 1

--- hashlife.py
+++ hashlife.py
@@ -28,27 +28,39 @@ class NaiveUniverse(Universe):
         self.m = m
         self.__cells = cells
 
+    def repr(self):
+        s = ""
+        for i in range(self.n):
+            for j in range(self.m):
+                if self.get(self.n - i - 1, j):
+                    s += "1 "
+                else:
+                    s += "0 "
+            s += "\n"
+        return s
+
     def get(self, i, j):
         return self.__cells[i][j]
 
-    def set(self, i, j, value):
-        self.__cells[i][j] = value
-
     def round(self):
+        next_round_cells = [sub[:] for sub in self.__cells]
         for i in range(self.n):
             for j in range(self.m):
                 n_neighbors = len(self.get_living_neighbors(i, j))
                 is_alive = self.get(i, j)
                 if not is_alive and n_neighbors == 3:
-                    self.set(i, j, True)
-                elif is_alive and not n_neighbors in [2, 3]:
-                    self.set(i, j, False)
-
+                    next_round_cells[i][j] = True
+                elif is_alive and n_neighbors in [2, 3]:
+                    next_round_cells[i][j] = True
+                else:
+                    next_round_cells[i][j] = False
+        self.__cells = next_round_cells
 
     def get_living_neighbors(self, i, j):
         neighbors = list()
-        for i_pointer in range(i-1, i+2):
-            for j_pointer in range(j-1, j+2):
-                if 0 <= i_pointer < self.n and 0 <= j_pointer < self.n and (i_pointer != i or j_pointer != j) and self.get(i_pointer, j_pointer):
+        for i_pointer in range(i - 1, i + 2):
+            for j_pointer in range(j - 1, j + 2):
+                if 0 <= i_pointer < self.n and 0 <= j_pointer < self.n and (
+                        i_pointer != i or j_pointer != j) and self.get(i_pointer, j_pointer):
                     neighbors.append((i_pointer, j_pointer))
         return neighbors
