32fc054c3608d72e135df03d909f608f9e9346b6
Kevin Messali <kevinmessali06@gmail.com>
2021-04-29 19:59:19 +0200

    Working on neighbors

--- hashlife.py
+++ hashlife.py
@@ -26,53 +26,29 @@ class NaiveUniverse(Universe):
     def __init__(self, n, m, cells):
         self.n = n
         self.m = m
-        self.cells = cells
+        self.__cells = cells
 
     def get(self, i, j):
-        return self.cells[self.n - i - 1][j]
+        return self.__cells[i][j]
 
     def set(self, i, j, value):
-        self.cells[self.n - i - 1][j] = value
+        self.__cells[i][j] = value
 
     def round(self):
         for i in range(self.n):
             for j in range(self.m):
-                neighbors = self.get_living_neighbors(i, j)
-                if not self.get(i, j) and len(neighbors) == 3:
+                n_neighbors = len(self.get_living_neighbors(i, j))
+                is_alive = self.get(i, j)
+                if not is_alive and n_neighbors == 3:
                     self.set(i, j, True)
-                elif self.get(i, j) and (len(neighbors) != 2 or len(neighbors) != 3):
+                elif is_alive and not n_neighbors in [2, 3]:
                     self.set(i, j, False)
+
 
     def get_living_neighbors(self, i, j):
         neighbors = list()
-        for i_pointer in range(i - 1, i + 2):
-            for j_pointer in range(j - 1, j + 2):
-                if 0 <= i_pointer < self.n and 0 <= j_pointer < self.m and (
-                        i_pointer != i or j_pointer != j) and self.get(i_pointer, j_pointer):
-                    print(self.n - i_pointer - 1, j_pointer)
+        for i_pointer in range(i-1, i+2):
+            for j_pointer in range(j-1, j+2):
+                if 0 <= i_pointer < self.n and 0 <= j_pointer < self.n and (i_pointer != i or j_pointer != j) and self.get(i_pointer, j_pointer):
+                    neighbors.append((i_pointer, j_pointer))
         return neighbors
-
-    # def round(self):
-    #     for i in range(self.n):
-    #         for j in range(self.m):
-    #             neighbors = self.get_living_neighbors(i, j)
-    #
-    #             if not self.get(i, j) and len(neighbors) == 3:
-    #                 self.set(i, j, True)
-    #             elif self.get(i, j) and (len(neighbors) != 2 or len(neighbors) != 3):
-    #                 self.set(i, j, False)
-    #
-    # def get(self, i, j):
-    #     return self.cells[i][self.m - j - 1]
-    #
-    # def set(self, i, j, value):
-    #     self.cells[i][self.m - j - 1] = value
-    #
-    # def get_living_neighbors(self, i, j):
-    #     neighbors = list()
-    #     for i_pointer in range(i - 1, i + 2):
-    #         for j_pointer in range(j - 1, j + 2):
-    #             if 0 <= i_pointer < self.n and 0 <= j_pointer < self.m and (i_pointer != i or j_pointer != j) \
-    #                     and self.get(i_pointer, j_pointer):
-    #                 neighbors.append((i_pointer, j_pointer))
-    #     return neighbors
